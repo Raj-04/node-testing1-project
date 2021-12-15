0f9d082359a6d6f1a05fa1cc3edddbfb898fa602
Raj-04 <1204patel.raj@gmail.com>
2021-12-15 13:25:05 -0500

    Seasons tests passing

--- index.test.js
+++ index.test.js
@@ -68,12 +68,48 @@ describe('[Exercise 5] Seasons', () => {
   beforeEach(() => {
     seasons = new utils.Seasons() // each test must start with fresh seasons
   })
-  // test('[9] the FIRST call of seasons.next returns "summer"', () => {})
-  // test('[10] the SECOND call of seasons.next returns "fall"', () => {})
-  // test('[11] the THIRD call of seasons.next returns "winter"', () => {})
-  // test('[12] the FOURTH call of seasons.next returns "spring"', () => {})
-  // test('[13] the FIFTH call of seasons.next returns again "summer"', () => {})
-  // test('[14] the 40th call of seasons.next returns "spring"', () => {})
+  test('[9] the FIRST call of seasons.next returns "summer"', () => {
+    const actual = seasons.next()
+    const expected = 'summer'
+    expect(actual).toBe(expected)
+  })
+  test('[10] the SECOND call of seasons.next returns "fall"', () => {
+    seasons.next()
+    const actual = seasons.next()
+    const expected = 'fall'
+    expect(actual).toBe(expected)
+  })
+  test('[11] the THIRD call of seasons.next returns "winter"', () => {
+    seasons.next()
+    seasons.next()
+    const actual = seasons.next()
+    const expected = 'winter'
+    expect(actual).toBe(expected)
+  })
+  test('[12] the FOURTH call of seasons.next returns "spring"', () => {
+    for (let i = 0; i < 3; i++) {
+      seasons.next()
+    }
+    const actual = seasons.next()
+    const expected = 'spring'
+    expect(actual).toBe(expected)
+  })
+  test('[13] the FIFTH call of seasons.next returns again "summer"', () => {
+    for (let i = 0; i < 4; i++) {
+      seasons.next()
+    }
+    const actual = seasons.next()
+    const expected = 'summer'
+    expect(actual).toBe(expected)
+  })
+  test('[14] the 40th call of seasons.next returns "spring"', () => {
+    for (let i = 0; i < 39; i++) {
+      seasons.next()
+    }
+    const actual = seasons.next()
+    const expected = 'spring'
+    expect(actual).toBe(expected)
+  })
 })
 
 describe('[Exercise 6] Car', () => {
